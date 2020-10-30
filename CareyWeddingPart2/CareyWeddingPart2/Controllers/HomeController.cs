62486a7b2c33d1bbf5794313f35c32fdbe7b502b
Christopher Nuckolls <chrislnuckolls@gmail.com>
2020-10-29 21:28:43 -0500

    Initial Commit

---
+++ CareyWeddingPart2/CareyWeddingPart2/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CareyWeddingPart2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Venue()
+        {
+            ViewBag.Message = "The Wedding Venue";
+
+            return View();
+        }
+
+        public ActionResult Accommodations()
+        {
+            ViewBag.Message = "Our Host Hotel";
+
+            return View();
+        }
+
+        public ActionResult ThingsToDo()
+        {
+            return View();
+        }
+    }
+}
---
+++ CareyWeddingPart2/CareyWeddingPart2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CareyWeddingPart2.MvcApplication" Language="C#" %>
